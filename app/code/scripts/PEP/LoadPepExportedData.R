96ca2a65d28ce06857444ed787283cd09ac20f0a
Bart Meddeler <bart.meddeler@radboudumc.nl>
2024-02-29 13:37:46 +0100

    additional JSON check to handle plain txt files

--- app/code/scripts/PEP/LoadPepExportedData.R
+++ app/code/scripts/PEP/LoadPepExportedData.R
@@ -4,48 +4,54 @@ library(writexl)
 library(readr)
 
 LoadExportedPEPData <- function(pepDataFolder, outputFolder){
-  subFolders <- list.dirs(pepDataFolder)[-1]
-  dfFiles <- data.frame()
-  
-  # Gather available files (data columns) from subfolders
-  for (subFolder in subFolders) {
-     id <- tail(strsplit(subFolder, "/")[[1]], 1)
-  
-     files <- list.files(subFolder)
-     if(length(files) > 0){
-       for (file in files){
-         dfFiles <- rbind(dfFiles, c(file))
-       }
-     }
-  }
-  dfFiles <- unique(dfFiles)
-  
-  # Iterate over files and save merged data per file/data column
-  for (i in 1:nrow(dfFiles)) {
-    df <- data.frame()
-
-    for (subFolder in subFolders) {
-      id <- tail(strsplit(subFolder, "/")[[1]], 1)
-      path <- paste0(subFolder, "/", dfFiles[i,])
-      if(file.exists(path)){
-        content <- read_file(path)
-        row <- data.frame()
-        print(content)
-        if(jsonlite::validate(content)){
-          data <- fromJSON(txt = content)
-          row <- data.frame(ID = id, data$crf, data$reports)
-          df <- bind_rows(df, row)
-        } else{
-          row <- data.frame(ID = id, content)
+  tryCatch(
+    {
+      subFolders <- list.dirs(pepDataFolder)[-1]
+      dfFiles <- data.frame()
+      
+      # Gather available files (data columns) from subfolders
+      for (subFolder in subFolders) {
+        id <- tail(strsplit(subFolder, "/")[[1]], 1)
+        files <- list.files(subFolder)
+        if(length(files) > 0){
+          for (file in files){
+            dfFiles <- rbind(dfFiles, c(file))
+          }
         }
-        df <- bind_rows(df, row)
       }
+      dfFiles <- unique(dfFiles)
+      
+      # Iterate over files and save merged data per file/data column
+      for (i in 1:nrow(dfFiles)) {
+        df <- data.frame()
+        print(dfFiles[i,])
+        
+        for (subFolder in subFolders) {
+          id <- tail(strsplit(subFolder, "/")[[1]], 1)
+          path <- paste0(subFolder, "/", dfFiles[i,])
+          if(file.exists(path)){
+            value <- read_file(path)
+            row <- data.frame()
+            if(jsonlite::validate(value) && grepl("{", value, fixed = TRUE)){
+              data <- fromJSON(txt = value)
+              row <- data.frame(ID = id, data$crf, data$reports)
+              df <- bind_rows(df, row)
+            } else{
+              row <- data.frame(ID = id, value)
+            }
+            df <- bind_rows(df, row)
+          }
+        }
+        if(dfFiles[i,] != ""){
+          xlsxPath <- paste0(outputFolder, "/", dfFiles[i,], ".xlsx")
+          write_xlsx(df, xlsxPath)
+        }
+      }
+      colnames(dfFiles) <- c("Filename")
+      return(dfFiles)
+    },
+    error = function(error) {
+      stop(error)
     }
-    if(dfFiles[i,] != ""){
-      xlsxPath <- paste0(outputFolder, "/", dfFiles[i,], ".xlsx")
-      write_xlsx(df, xlsxPath)
-    }
-  }
-  
-  return(dfFiles)
+  )
 }
